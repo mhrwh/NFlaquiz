362c7fad590d78b378eeb54f9c79de1f9f1f4d26
mhrwh <i072ff@yamaguchi-u.ac.jp>
2022-07-04 14:00:27 +0900

    マップ切り替えまで

---
+++ ui/src/data/mapData.js
@@ -0,0 +1,54 @@
+export default{
+    data: {
+        colorWeight: {
+          name: '',
+          format: ' ',
+        },
+        description1: {
+          name: '',
+          format: '{0}'
+        },
+        description2: {
+          name: '',
+          format: '{0} ',
+        },
+        description3: {
+          name: '',
+          format: '{0}'
+        },
+        description4: {
+          name: '',
+          format: '{0}'
+        },
+        description5: {
+          name: '',
+          format: '{0}'
+        },
+        description6: {
+          name: '',
+          format: '{0}'
+        },
+        description7: {
+          name: '',
+          format: '{0}'
+        },
+        description8: {
+          name: '',
+          format: '{0}'
+        }
+    },
+    applyData: 'colorWeight',
+    values: {
+        JP: {
+          colorWeight: 1,
+          description1: "世界第3位の国土と人口を持つ。雄大な大自",
+          description2: "然や魅力的な観光スポットが多いことで有名。",
+          description3: "公用語は存在せず、国内では300種類以上",
+          description4 :"の言語が使用されていると言われている。",
+          description5: "陸上国境は2か国との間にしかないが、海上",
+          description6:"国境はロシアなど多くの国との間に存在する。",
+          description7:"",
+          description8:""
+        }
+      }
+};
