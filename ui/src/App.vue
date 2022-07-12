7b158c0cb4e4912d14f912f681ed433b807b01f8
mhrwh <i072ff@yamaguchi-u.ac.jp>
2022-07-12 15:54:58 +0900

    構成変更

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,23 +1,9 @@
 <template>
-  <button @click="currentComponent = 'CorrectAnswersRateMap'">正答率</button>
-  <button @click="currentComponent = 'BookMarkMap'">ブックマーク</button>
-  <CorrectAnswersRateMap v-if="currentComponent === 'CorrectAnswersRateMap'"></CorrectAnswersRateMap>
-  <BookMarkMap v-if="currentComponent === 'BookMarkMap'"></BookMarkMap>
+  <router-view />
 </template>
 
 <script>
-import CorrectAnswersRateMap from './components/correctAnswersRateMap.vue'
-import BookMarkMap from './components/bookMarkMap.vue'
 export default {
   name: 'App',
-  components: {
-    CorrectAnswersRateMap,
-    BookMarkMap
-  },
-  data() {
-    return {
-      currentComponent: "CorrectAnswersRateMap",
-    };
-  }
 }
 </script>
---
+++ ui/src/pages/TopPage.vue
@@ -0,0 +1,29 @@
+<template>
+<v-container>
+    <v-card class="md:mx-4 my-4 px-1 py-20">
+      <h1 class="text-2xl md:text-4xl text-center">オープンデータカタログ検索</h1>
+    </v-card>
+
+  </v-container>
+  <button @click="currentComponent = 'CorrectAnswersRateMap'">正答率</button>
+  <button @click="currentComponent = 'BookMarkMap'">ブックマーク</button>
+  <CorrectAnswersRateMap v-if="currentComponent === 'CorrectAnswersRateMap'"></CorrectAnswersRateMap>
+  <BookMarkMap v-if="currentComponent === 'BookMarkMap'"></BookMarkMap>
+</template>
+
+<script>
+import CorrectAnswersRateMap from '@/components/correctAnswersRateMap.vue'
+import BookMarkMap from '@/components/bookMarkMap.vue'
+export default {
+  name: 'TopPage',
+  components: {
+    CorrectAnswersRateMap,
+    BookMarkMap
+  },
+  data() {
+    return {
+      currentComponent: "CorrectAnswersRateMap",
+    };
+  }
+}
+</script>
