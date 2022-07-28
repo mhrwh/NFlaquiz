61f26ff28a2db39fbb66c0d4aa1b63327cf8a0ad
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-07-28 13:25:39 +0900

    v-drag

--- ui/src/main.js
+++ ui/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import drag from "v-drag"
 //import { loadFonts } from './plugins/webfontloader'
 
 //loadFonts()
@@ -13,4 +14,7 @@ createApp(App)
   .use(router)
   .use(store)
   .use(vuetify)
+  .use(drag, {
+    // global configuration
+  })
   .mount('#app')
