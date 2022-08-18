42e33e720c84af59c6f304f0c80e615ec4c6c032
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-08-18 16:55:50 +0900

    mapData追加

---
+++ ui/src/data/mapData.js
@@ -0,0 +1,44 @@
+export default{
+    data: {
+        colorWeight: {
+          name: '',
+          format: ' ',
+        },
+        description1: {
+          name: '',
+          format: '{0}'
+        },
+        description2: {
+          name: '',
+          format: '{0} ',
+        },
+        description3: {
+          name: '',
+          format: '{0}'
+        },
+        description4: {
+          name: '',
+          format: '{0}'
+        },
+        description5: {
+          name: '',
+          format: '{0}'
+        },
+        description6: {
+          name: '',
+          format: '{0}'
+        },
+        description7: {
+          name: '',
+          format: '{0}'
+        },
+        description8: {
+          name: '',
+          format: '{0}'
+        }
+    },
+    applyData: 'colorWeight',
+    values: {
+       
+      }
+};
