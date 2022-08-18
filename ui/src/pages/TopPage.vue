49d5ac6fb9d665948e92b06ab4bff415aab85c2f
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-08-19 00:08:43 +0900

    svgmap追加

--- ui/src/pages/TopPage.vue
+++ ui/src/pages/TopPage.vue
@@ -1,18 +1,32 @@
 <template>
-
+  <div id="map">
+    <WorldMap v-if="mapMode==='correctAnswersRate'" v-bind:mapMode="mapMode"></WorldMap>
+    <WorldMap v-if="mapMode==='bookMark'" v-bind:mapMode="mapMode"></WorldMap>
+      
+  </div>
 </template>
 
 <script>
-
-
+import WorldMap from '@/components/worldMap.vue'
+import { ref } from 'vue'
 export default {
   name: 'TopPage',
   components: {
+    WorldMap,
     
+},
+  setup() {
+    const mapMode = ref("correctAnswersRate")
+    return {
+      mapMode,
+    };
   }
-}
+};
 </script>
 
 <style>
+#map{
+  position: relative;
+};
 
 </style>
