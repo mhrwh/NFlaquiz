2d1b78ac2b5dc7dd86122609e43c8a20b9655787
raylicola <rope.champ.109@gmail.com>
2022-10-30 12:56:45 +0900

    クイズの選択処理を作成

--- api/controllers/quiz_controller.go
+++ api/controllers/quiz_controller.go
@@ -1,15 +1,15 @@
 package controllers
 
 import (
-  "log"
-  "net/http"
-  "strconv"
+	"log"
+	"net/http"
+	"strconv"
 
-  "github.com/gin-gonic/gin"
-  "github.com/raylicola/NFlaquiz/database"
-  "github.com/raylicola/NFlaquiz/models"
-  "github.com/raylicola/NFlaquiz/utils"
-  mapset "github.com/deckarep/golang-set/v2"
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/gin-gonic/gin"
+	"github.com/raylicola/NFlaquiz/database"
+	"github.com/raylicola/NFlaquiz/models"
+	"github.com/raylicola/NFlaquiz/utils"
 )
 
 // 検索条件に当てはまるクイズを10問選ぶ
@@ -38,14 +38,14 @@ func SelectQuiz(c *gin.Context) {
   var flag_colors []models.FlagColor
   var results []models.Result
 
-  // 初めに全ての国を候補とする
+  // まずは全ての国を候補とする
   options := mapset.NewSet[string]()
   database.DB.Find(&countries)
   for _, v := range countries {
     options.Add(v.ID)
   }
 
-  // 地域が該当する国を絞る
+  // 該当地域の国を絞る
   if len(areas) != 0 {
     database.DB.Where("area_id in (?)", areas).Find(&countries)
     selected := mapset.NewSet[string]()
@@ -55,7 +55,7 @@ func SelectQuiz(c *gin.Context) {
     options = options.Intersect(selected)
   }
 
-  // 色が完全一致する国を絞る
+  // 国旗の色が完全一致する国を絞る
   if len(colors) != 0 {
     database.DB.Select("country_id").Where("color_id in (?)", colors).
       Group("country_id").Having("count(*) = ?", len(colors)).Find(&flag_colors)
@@ -66,7 +66,7 @@ func SelectQuiz(c *gin.Context) {
     options = options.Intersect(selected)
   }
 
-  // 該当ユーザーがブックマークした国を絞る
+  // ユーザーがブックマークした国を絞る
   if  (err == nil) && (bookmark == 1) {
     database.DB.Where("user_id=?", user.ID).Where("bookmark=?", bookmark).Find(&results)
     selected := mapset.NewSet[string]()
@@ -76,10 +76,46 @@ func SelectQuiz(c *gin.Context) {
     options = options.Intersect(selected)
   }
 
-  log.Println(options)
+  // 検索のための型変換
+  var country_array []string
+  options.Each(func(country string) bool {
+    country_array = append(country_array, country)
+    return false
+  })
 
   // 絞り込んだ国からクイズを選択
   var quizzes []models.Quiz
+
+  if err != nil {
+    // 未ログインユーザーの場合, ランダムにクイズを選択
+    database.DB.Where("quizzes.country_id in (?)", country_array).
+      Order("rand()").Limit(10).Find(&quizzes)
+  } else {
+    // ログイン済みユーザーの場合, 未回答のものを優先して選択
+    query := database.DB.Joins("left outer join results on quizzes.country_id = results.country_id").
+      Where("quizzes.country_id in (?)", country_array).
+      Where("quizzes.country_id not in (?)", database.DB.Table("results").Select("country_id").Where("user_id=?", user.ID)).
+      Order("rand()").Limit(10).Find(&quizzes)
+
+    // 未回答問題数が10未満の場合, 正答率が低い順に残りを埋める
+    left := int(10 - query.RowsAffected)
+    if left > 0 {
+      var low_weight_quizzes []models.Quiz
+      low_weight_query := database.DB.Joins("left outer join results on quizzes.country_id = results.country_id").
+        Where("quizzes.country_id in (?)", country_array).
+        Where("user_id = ?", user.ID).
+        Order("weight").Limit(left).Find(&low_weight_quizzes)
+
+      log.Println(quizzes[0])
+      log.Println(low_weight_quizzes)
+
+      database.DB.Raw(
+				"(?) UNION (?)",
+				query,
+				low_weight_query,
+			).Scan(&quizzes)
+    }
+  }
 
   c.JSON(http.StatusOK, gin.H{"quizzes": quizzes})
 }
