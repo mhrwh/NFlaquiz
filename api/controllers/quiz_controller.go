1d5bf716d578ec51034fff76c2e0093e35578cc6
raylicola <rope.champ.109@gmail.com>
2022-10-30 11:07:26 +0900

    refactor: 冗長性排除のため,  都度積集合をとる処理に変更

--- api/controllers/quiz_controller.go
+++ api/controllers/quiz_controller.go
@@ -34,47 +34,51 @@ func SelectQuiz(c *gin.Context) {
   bookmark, _ := strconv.Atoi(req.Bookmark)
   user, err := utils.AuthUser(c)
 
-  // 地域が該当する国
-  if len(areas) == 0 {
-    areas = append(areas, "Africa", "Asia", "Europe", "LatinAmericaandCaribbean",
-                          "MiddleEast", "NorthAmerica", "Oceania")
-  }
   var countries []models.Country
-  database.DB.Where("area_id in (?)", areas).Find(&countries)
-  options1 := mapset.NewSet[string]()
-  for _, v := range countries {
-    options1.Add(v.ID)
-  }
-
-  // 色が完全一致する国
   var flag_colors []models.FlagColor
-  database.DB.Select("country_id").Where("color_id in (?)", colors).
-    Group("country_id").Having("count(*) >= ?", len(colors)).Find(&flag_colors)
-  options2 := mapset.NewSet[string]()
-  for _, v := range flag_colors {
-    options2.Add(v.CountryID)
+  var results []models.Result
+
+  // 初めに全ての国を候補とする
+  options := mapset.NewSet[string]()
+  database.DB.Find(&countries)
+  for _, v := range countries {
+    options.Add(v.ID)
   }
 
-  // 該当ユーザーがブックマークした国
-  var results []models.Result
-  options3 := mapset.NewSet[string]()
+  // 地域が該当する国を絞る
+  if len(areas) != 0 {
+    database.DB.Where("area_id in (?)", areas).Find(&countries)
+    selected := mapset.NewSet[string]()
+    for _, v := range countries {
+      selected.Add(v.ID)
+    }
+    options = options.Intersect(selected)
+  }
+
+  // 色が完全一致する国を絞る
+  if len(colors) != 0 {
+    database.DB.Select("country_id").Where("color_id in (?)", colors).
+      Group("country_id").Having("count(*) = ?", len(colors)).Find(&flag_colors)
+    selected := mapset.NewSet[string]()
+    for _, v := range flag_colors {
+      selected.Add(v.CountryID)
+    }
+    options = options.Intersect(selected)
+  }
+
+  // 該当ユーザーがブックマークした国を絞る
   if  (err == nil) && (bookmark == 1) {
     database.DB.Where("user_id=?", user.ID).Where("bookmark=?", bookmark).Find(&results)
+    selected := mapset.NewSet[string]()
     for _, v := range results {
-      options3.Add(v.CountryID)
+      selected.Add(v.CountryID)
     }
-  } else {
-    database.DB.Find(&countries)
-    for _, v := range countries {
-      options3.Add(v.ID)
-    }
+    options = options.Intersect(selected)
   }
 
-  // 全ての絞り込み条件に一致した国
-  options :=options1.Intersect(options2).Intersect(options3)
   log.Println(options)
 
-  // クイズを選択
+  // 絞り込んだ国からクイズを選択
   var quizzes []models.Quiz
 
   c.JSON(http.StatusOK, gin.H{"quizzes": quizzes})
