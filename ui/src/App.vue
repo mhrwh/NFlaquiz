f556091684f3999615af8a457c5140c5e0992d1f
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2023-02-02 05:30:16 +0900

    ヘッダー追加

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <PageHeader />
   <router-view />
 </template>
 
 <script>
+import PageHeader from './components/pageHeader.vue';
 export default {
   name: 'App',
+  components: {
+    PageHeader
+  },
 }
 </script>
