b10c0bb7ce20c81dc5b12142471727cf13241483
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2023-02-02 05:29:20 +0900

    ヘッダー作成

---
+++ ui/src/components/pageHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="navbar flex-nowrap navbar-light header">
+    <a class="navbar-brand">
+      <img src="@/assets/NFlaquizlogo.png" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
+      NFlaquiz
+    </a>
+    <div class="navbar-brand" v-if="$route.name==='TopPage'">
+      <button class="btn btn-header" data-toggle="modal" data-target="#loginModal" data-backdrop="false" v-if="!auth">
+          <i class="bi bi-person-fill" />
+      </button>
+      <button class="btn btn-header" @click="logout" v-else-if="auth">
+        <i class="bi bi-person-check-fill" />
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import axios from 'axios';
+
+export default {
+  name: 'PageHeader',
+  setup() {
+    const store = useStore();
+    const auth = computed(() => store.state.auth);
+
+    const logout = async () => {
+      axios.get('http://localhost:8888/logout')
+        .then(() => {
+          store.dispatch("setAuth", false);
+          store.dispatch('setMapMode', 'correctAnswersRate');
+          location.reload();
+        })
+    };
+
+    return {
+      auth,
+      logout,
+    };
+  },
+}
+</script>
+
+<style>
+.header {
+  height: 50px;
+}
+.btn-header i {
+  color: #FFCF32;
+  font-size: 26px;
+}
+</style>
