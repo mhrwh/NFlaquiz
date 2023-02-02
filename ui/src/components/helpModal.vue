f986931a3fc8c8855ca5907bc83056e627b7ecbf
seki <k011@ymail.ne.jp>
2023-02-02 19:09:15 +0900

    段落にした

--- ui/src/components/helpModal.vue
+++ ui/src/components/helpModal.vue
@@ -2,7 +2,7 @@
   <div class="modal fade" id="helpModal" tabindex="-1" aria-labelledby="helpModalTitle" aria-hidden="false">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content help-modal">
-        <div class="modal-header p-0 border-bottom-0">
+        <div class="modal-header px-0 border-bottom-0">
           <h2 class="modal-title" id="helpModalTitle">ヘルプ</h2>
         </div>
         <div class="modal-body p-0">
@@ -18,21 +18,38 @@
               </a>
             </li>
           </ul>
-          <div class="tab-content">
+          <div class="tab-content tab-content-area">
             <div class="tab-pane fade show active" id="tab1" v-if="selectedTab === 'tab1'">
-              <i class="bi bi-person-fill" />
-              ログインボタン<br>
-              <i class="bi bi-layout-sidebar-inset" />
-              マップ切り替えボタン（正答率モード）<br>
-              <i class="bi bi-layout-sidebar-inset-reverse" />
-              マップ切り替えボタン（ブックマークモード）<br>
-              <i class="bi bi-bookmark-heart" />
-              ブックマーク編集ボタン<br>
-              <i class="bi bi-flag-fill" />
-              クイズボタン
+              <h4>
+                <i class="bi bi-layout-sidebar-inset" />
+                マップ切り替え
+              </h4>
+              <div class="text-danger">※この機能はログイン時のみ使用可能です。</div>
+              マップには赤系統色で正答率を表すモードと<br>
+              緑系統色でブックマークした国を示すモードの2種類があります。<br>
+
+
+
+              <p class="h4">
+                <i class="bi bi-bookmark-heart" />
+                ブックマーク編集
+              </p>
+              <div class="text-danger">※この機能はログイン時のみ使用可能です。</div>
+              問題解答後にブックマークした国をブックマークしていない状態に戻すことができます。
+
+              <p class="h4">
+                <i class="bi bi-flag-fill" />
+                クイズ絞り込み
+              </p>
+              クイズを始める前に国の地域と国旗の色で出題される国を絞り込むことができます。
+              出題される問題は10問です。
+              a<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>a
+
+
+
             </div>
             <div class="tab-pane fade" id="tab2" v-if="selectedTab === 'tab2'">
-              ログインすることでブックマーク機能が使えたり、<br>
+              ログインすることでブックマーク機能が使えたり、
               正答率・ブックマークした国をマップに反映することができます。
             </div>
           </div>
@@ -59,13 +76,13 @@ export default {
 }
 
 .modal-content {
-  height: 50%;
+  height: 70%;
   display: flex;
   flex-direction: column;
 }
 
-.modal-body {
-  flex: 1;
+.tab-content-area {
+  height: 65%;
   overflow-y: auto;
 }
 </style>
