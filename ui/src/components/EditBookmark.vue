3bfa6f04778388de60f8284480ad81054f7305c9
seki <k011@ymail.ne.jp>
2023-02-02 18:12:56 +0900

    ボーダーを追加

--- ui/src/components/EditBookmark.vue
+++ ui/src/components/EditBookmark.vue
@@ -2,7 +2,7 @@
   <div class="modal fade" id="editBookmark" tabindex="-1" aria-labelledby="editBookmarkTitle" data-backdrop="static"
     aria-hidden="false">
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content edit-bookmark-modal">
+      <div class="modal-content edit-bookmark-modal pb-5">
         <div class="modal-header px-4 border-bottom-0">
           <h2 class="modal-title" id="editBookmarkTitle">ブックマーク編集</h2>
           <button type="button" class="close modal-close" data-dismiss="modal" @click="reload" aria-label="Close">
@@ -10,26 +10,28 @@
           </button>
         </div>
         <div class="modal-body p-0">
-          <div v-if="bookMarks.length > 0" class="table-responsive mb-4 bookmarks-table bookmarks-country-name-col"
-            style="overflow-y: scroll ">
-            <table class="table table-hover">
-              <tbody>
-                <tr v-for="(value, index) in bookMarks" :key="value.name"
-                  :class="{ 'even': index % 2 === 0, 'odd': index % 2 !== 0 }">
-                  <td scope="col" class="align-middle w-70pct pl-5">
-                    {{ value.name }}
-                  </td>
-                  <div class="text-right">
-                    <td scope="col" class="align-middle pr-5 w-30pct ">
-                      <button class="btn btn-danger" v-on:click="deleteBookmark(value.id)">削除</button>
+          <div class="border-top border-bottom border-secondary">
+            <div v-if="bookMarks.length > 0" class="table-responsive bookmarks-table bookmarks-country-name-col mb-0"
+              style="overflow-y: scroll ">
+              <table class="table table-hover">
+                <tbody>
+                  <tr v-for="(value, index) in bookMarks" :key="value.name"
+                    :class="{ 'even': index % 2 === 0, 'odd': index % 2 !== 0 }">
+                    <td scope="col" class="align-middle w-70pct pl-5">
+                      {{ value.name }}
                     </td>
-                  </div>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div v-if="bookMarks.length == 0" class="mb-4  pl-5 bookmarks-table">
-            ブックマークがありません
+                    <div class="text-right">
+                      <td scope="col" class="align-middle pr-5 w-30pct ">
+                        <button class="btn btn-danger" v-on:click="deleteBookmark(value.id)">削除</button>
+                      </td>
+                    </div>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div v-if="bookMarks.length == 0" class="pl-5 bookmarks-table">
+              ブックマークがありません
+            </div>
           </div>
         </div>
       </div>
