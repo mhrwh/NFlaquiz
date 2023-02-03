c27cc2d63202c0530ec416c5f112c3b98d674b4b
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2023-02-03 13:14:57 +0900

    style修正

--- ui/src/components/iconMenu.vue
+++ ui/src/components/iconMenu.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="btn-toolbar">
     <div class="btn-group" v-if="auth">
-      <button class="btn btn-light btn-circle" style="border-radius: 50%" @click="mapSwitch('bookMark')"
+      <button class="btn btn-light btn-circle" @click="mapSwitch('bookMark')"
         v-if="mapMode == 'correctAnswersRate'">
         <i class="bi bi-layout-sidebar-inset" />
       </button>
-      <button class="btn btn-light btn-circle" style="border-radius: 50%" @click="mapSwitch('correctAnswersRate')"
+      <button class="btn btn-light btn-circle" @click="mapSwitch('correctAnswersRate')"
         v-if="mapMode == 'bookMark'">
         <i class="bi bi-layout-sidebar-inset-reverse" />
       </button>
     </div>
     <div class="btn-group" v-if="auth" data-toggle="modal" data-target="#editBookmark">
-      <button class="btn btn-light btn-circle" style="border-radius: 50%">
+      <button class="btn btn-light btn-circle">
         <i class="bi bi-bookmark-heart" />
       </button>
     </div>
     <div class="btn-group">
-      <button class="btn btn-light btn-circle" data-toggle="modal" data-target="#quizFilterModal"
-        style="border-radius: 50%">
+      <button class="btn btn-light btn-circle" data-toggle="modal" data-target="#quizFilterModal">
         <i class="bi bi-flag-fill" />
       </button>
     </div>
@@ -56,24 +55,6 @@ export default {
   z-index: 1;
   margin-top: 45px;
   margin-left: 46px;
-}
-
-.btn-circle {
-  width: 45px;
-  height: 45px;
-  text-align: center;
-  padding: 0;
-  border-radius: 50%;
-  margin-right: 76px;
-}
-
-.btn-circle i {
-  position: relative;
-  color: #FFCF32;
-  font-size: 26px;
-  display: flex;
-  justify-content: center;
-  top: 2px;
 }
 
 </style>
